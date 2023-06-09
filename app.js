6721568dda0e99efbc1604a15531d43401324c4a
camila-salvia <camilasalvia2@gmail.com>
2023-06-09 19:46:33 -0300

    inicio

---
+++ app.js
@@ -0,0 +1,14 @@
+/*console.log('Hola mundo');
+window.addEventListener('load',() => {
+    const submitButton = document.querySelector('#submit');
+    submitButton.addEventListener('click',(event) => {
+        event.preventDefault();
+        const name = document.querySelector('#name').value;
+        const email = document.querySelector('#email').value;
+        const message = document.querySelector('#message').value;
+    
+        console.log(name, email, message);
+    })
+})
+*/
+
